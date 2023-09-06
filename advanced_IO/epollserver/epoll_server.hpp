68e2a70f08ae3bdd0bdf298b18461e8799b30f29
lirendada <2916776007@qq.com>
2023-09-06 13:32:59 +0800

    epoll代码完善

--- advanced_IO/epollserver/epoll_server.hpp
+++ advanced_IO/epollserver/epoll_server.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <sys/epoll.h>
+#include <fcntl.h>
 #include "sock.hpp"
 
 namespace epoll_space
@@ -12,6 +13,13 @@ namespace epoll_space
 
     class epoll_server
     {
+    private:
+        uint16_t _port;
+        int _listensock;
+        int _epfd;                   // epoll模型的文件描述符
+        int _num;                    // 就绪事件数组的大小
+        struct epoll_event* _events; // 就绪事件数组
+
     public:
         epoll_server(uint16_t port = default_port, int num = defalult_num)
             : _port(port), _num(num)
@@ -30,17 +38,8 @@ namespace epoll_space
             }
             logMessage(Level::NORMAL, "create epoll success");
 
-            // 3. 添加_listensock到epoll中，并且关心可读事件
-            struct epoll_event in;
-            in.data.fd = _listensock;
-            in.events = EPOLLIN;
-            int ret = epoll_ctl(_epfd, EPOLL_CTL_ADD, _listensock, &in);
-            if(ret == -1)
-            {
-                logMessage(Level::ERROR, "add _listensock error");
-                exit(EPOLL_CTL_ERR);
-            }
-            logMessage(Level::NORMAL, "add _listensock success");
+            // 3. 添加_listensock到epoll中，并且关心可读事件和启动ET模式
+            AddFD(_listensock, true);
 
             // 4. 开辟就绪事件数组的空间
             _events = new struct epoll_event[_num];
@@ -77,23 +76,112 @@ namespace epoll_space
                     logMessage(Level::NORMAL, "一共有%d个事件就绪", n);
                     handler(n);
                 }
+                sleep(1);
             }
         }
 
         void handler(int ready_num)
         {
-            char buffer[1024];
             for(int i = 0; i < ready_num; ++i)
             {
-                
+                // 这里遍历的事件都是就绪的！
+                // 1. 首先获取事件类型以及文件描述符
+                uint32_t event = _events[i].events;
+                int fd = _events[i].data.fd;
+
+                // 2. 根据不同的事件类型以及文件描述符来做不同的业务处理
+                if(fd == _listensock && (event & EPOLLIN))
+                {
+                    // 如果是_listensock并且是可读事件，则获取新链接
+                    Accepter();
+                }
+                else if(event & EPOLLIN)
+                {
+                    // 如果是普通文件描述符的可读事件，我们就接收信息
+                    Receiver(fd);
+                }
+                else
+                {
+                    // 如果是普通文件描述符的可写事件等等，我们这里不做处理，等后面将reactor的时候一起写
+                }
             }
         }
     private:
-        uint16_t _port;
-        int _listensock;
+        // 设置文件描述符为阻塞模式
+        void SetNonBlocking(int fd)
+        {
+            // 1. 先获取文件描述符标记
+            int old_option = fcntl(fd, F_GETFL);
 
-        int _epfd;                   // epoll模型的文件描述符
-        int _num;                    // 就绪事件数组的大小
-        struct epoll_event* _events; // 就绪事件数组
+            // 2. 设为非阻塞模式
+            int new_option =old_option | O_NONBLOCK;
+            fcntl(fd, new_option);
+        }
+        
+        // 添加文件描述符到epoll模型中，如果有必要的话还可以设置为ET模式
+        void AddFD(int fd, bool enable_ET)
+        {
+            // 1. 设置为可读事件
+            struct epoll_event in;
+            in.data.fd = fd;
+            in.events = EPOLLIN;
+
+            // 2. 看看是否需要设置为ET模式
+            if(enable_ET)
+                in.events |= EPOLLET;
+            
+            // 3. 添加到epoll模型中
+            int ret = epoll_ctl(_epfd, EPOLL_CTL_ADD, fd, &in);
+            if(ret == -1)
+            {
+                logMessage(Level::ERROR, "epoll_ctl error, fd: %d, errno: %d, why: %s", fd, errno, strerror(errno));
+                exit(EPOLL_CTL_ERR);
+            }
+            logMessage(Level::NORMAL, "epoll_ctl success, fd: %d", fd);
+
+            // 4. 设置为非阻塞模式
+            SetNonBlocking(fd);
+        }
+
+        void Accepter()
+        {
+            // 1. 获取新连接
+            std::string clientip;
+            uint16_t clientport;
+            int newfd = sock::Accept(_listensock, &clientip, &clientport);
+
+            // 2. 将该新连接交给epoll模型管理
+            AddFD(newfd, true);;
+        }
+
+        void Receiver(int fd)
+        {
+            char buffer[1024];
+            ssize_t n = recv(fd, buffer, sizeof(buffer) - 1, 0);
+
+            if(n == -1)
+            {
+                // 读取发生错误，将该事件从epoll模型中去除，然后关闭
+                epoll_ctl(_epfd, EPOLL_CTL_DEL, fd, nullptr);
+                close(fd);
+                logMessage(Level::ERROR, "receive error, fd: %d, errno: %d, why: %s", fd, errno, strerror(errno));
+            }
+            else if(n == 0)
+            {
+                // 读到0表示请求断开连接，也是一样将该事件从epoll模型中去除，然后关闭
+                epoll_ctl(_epfd, EPOLL_CTL_DEL, fd, nullptr);
+                close(fd);
+                logMessage(Level::NORMAL, "receive close, fd: %d", fd);
+            }
+            else
+            {
+                buffer[n] = 0;
+                logMessage(Level::NORMAL, "receive success, fd: %d, 内容: %s", fd, buffer);
+
+                // 这里做简单的回响处理
+                std::string response = "响应: " + std::string(buffer);
+                send(fd, response.c_str(), response.size(), 0);
+            }
+        }
     };
 }
