e9bb903e1fb1e137e7daa1b0619360268e829a89
lirendada <2916776007@qq.com>
2023-09-23 21:41:25 +0800

    日志宏编写

--- one-thread-one-loop/source/main.cc
+++ one-thread-one-loop/source/main.cc
@@ -2,47 +2,8 @@
 
 int main()
 {
-    Buffer buf;
-
-/*
-    // 测试读取一行数据问题
-    for(int i = 0; i < 300; ++i)
-    {
-        std::string str = "lirendada tthh!" + std::to_string(i) + '\n';
-        buf.write_string_andMove(str);
-    }
-    while(buf.get_sizeof_read() > 0)
-        std::cout << buf.get_line_andMove() << std::endl;
-*/
-
-/*
-    // 测试扩容问题
-    for(int i = 0; i < 300; ++i)
-    {
-        std::string str = "lirendada tthh!" + std::to_string(i) + '\n';
-        buf.write_string_andMove(str);
-    }
-    std::string tmp;
-    tmp = buf.read_to_string_andMove(buf.get_sizeof_read());
-    std::cout << tmp << std::endl;
-*/
-
-/*
-    // 测试简单的读写操作
-    buf.write_string_andMove(str); // 进行写入
-    Buffer buf1;
-    buf1.write_Buffer_andMove(buf);
-
-    // 对buf进行读取
-    std::string tmp;
-    tmp = buf.read_to_string_andMove(buf.get_sizeof_read());
-
-    std::cout << tmp << std::endl;
-    std::cout << buf.get_sizeof_read() << std::endl;
-    std::cout << buf1.get_sizeof_read() << std::endl;
-
-    buf.write_data_andMove("hha", 3);
-    std::cout << buf.get_sizeof_read() << std::endl;
-*/
+    ILOG("liansdl");
+    DLOG("liren");
+    ELOG("hhha%s", "olainwd");
     return 0;
 }
--- one-thread-one-loop/source/server.hpp
+++ one-thread-one-loop/source/server.hpp
@@ -1,10 +1,33 @@
-#pragma once
+#ifndef __MY_SERVER_H__
+#define __MY_SERVER_H__
 #include <iostream>
 #include <vector>
 #include <cassert>
 #include <cstring>
+#include <cstdio>
+#include <pthread.h>
+#include <time.h>
 
 const static uint64_t MAX_BUFFER_SIZE = 1024;
+
+#define INF 0    // 提示型等级
+#define DEBUG 1  // 调试型等级
+#define ERROR 2  // 错误型等级
+#define DEFAULT_LOG_LEVEL DEBUG  // 默认的日志等级
+
+#define LOG(level, format, ...) do{\
+    if(DEFAULT_LOG_LEVEL > level) break;\
+    char timebuffer[128];\
+    time_t timestamp = time(NULL);\
+    struct tm* timeinfo = localtime(&timestamp);\
+    strftime(timebuffer, sizeof(timebuffer), "%Y-%m-%d %H:%M:%S", timeinfo);\
+    fprintf(stdout, "[%p %s %s:%d] " format "\n", (void*)pthread_self(), timebuffer, __FILE__, __LINE__, ##__VA_ARGS__);\
+}while(0)
+
+// 将等级和日志打印封装起来
+#define ILOG(format, ...) LOG(INF,   format, ##__VA_ARGS__)
+#define DLOG(format, ...) LOG(DEBUG, format, ##__VA_ARGS__)
+#define ELOG(format, ...) LOG(ERROR, format, ##__VA_ARGS__)
 
 class Buffer
 {
@@ -217,3 +240,5 @@ private:
         return _reader;
     }
 };
+
+#endif
