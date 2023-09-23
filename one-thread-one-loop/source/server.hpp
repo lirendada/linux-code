76a8b29bfacf3a36d777b04e80e1554b41233743
lirendada <2916776007@qq.com>
2023-09-23 23:09:48 +0800

    socket模块代码

--- one-thread-one-loop/source/server.hpp
+++ one-thread-one-loop/source/server.hpp
@@ -5,7 +5,13 @@
 #include <cassert>
 #include <cstring>
 #include <cstdio>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <time.h>
 
 const static uint64_t MAX_BUFFER_SIZE = 1024;
@@ -39,20 +45,20 @@ public:
     Buffer() :_reader(0), _writer(0), _buffer(MAX_BUFFER_SIZE) {}
     
     /////////////////////////////  写操作 /////////////////////////////
-    // 获取当前写入的起始地址
+    // 1. 获取当前写入的起始地址
     char* start_of_write()
     {
         return &(*_buffer.begin()) + _writer;
     }
 
-    // 获取可写数据的大小
+    // 2. 获取可写数据的大小
     uint64_t get_sizeof_write()
     {
         // 即开头空闲空间 + 结尾空闲空间
         return get_head_free_size() + get_tail_free_size();
     }
 
-    // 将写位置向后移动
+    // 3. 将写位置向后移动
     void push_writer_back(uint64_t size)
     {
         // 向后移动的大小，必须小于当前后边的空闲空间大小
@@ -60,7 +66,7 @@ public:
         _writer += size;
     }
 
-    // 写入数据操作（将data中数据写到缓冲区中）
+    // 4. 写入数据操作（将data中数据写到缓冲区中）
     void write_data(const void* data, uint64_t size)
     {
         // 1. 确保空间足够
@@ -73,40 +79,40 @@ public:
         std::copy(tmp, tmp + size, start_of_write());
     }
 
-    // 写入数据操作，并让写入位置向后移动
+    // 5. 写入数据操作，并让写入位置向后移动
     void write_data_andMove(const void* data, uint64_t size)
     {
         write_data(data, size);
         push_writer_back(size);
     }
 
-    // 写入string类型数据操作
+    // 6. 写入string类型数据操作
     void write_string(const std::string& data)
     {
         return write_data(data.c_str(), data.size());
     }
 
-    // 写入string类型数据操作，并让写入位置向后移动
+    // 7. 写入string类型数据操作，并让写入位置向后移动
     void write_string_andMove(const std::string& data)
     {
         write_string(data);
         push_writer_back(data.size());
     }
 
-    // 写入Buffer类型数据操作
+    // 8. 写入Buffer类型数据操作
     void write_Buffer(Buffer& data)
     {
         return write_data(data.start_of_read(), data.get_sizeof_read());
     }
 
-    // 写入Buffer类型数据操作，并让写入位置向后移动
+    // 9. 写入Buffer类型数据操作，并让写入位置向后移动
     void write_Buffer_andMove(Buffer& data)
     {
         write_Buffer(data);
         push_writer_back(data.get_sizeof_read());
     }
 
-    // 确保可写空间足够大（整体空闲空间够了就移动数据，否则就扩容）
+    // 10. 确保可写空间足够大（整体空闲空间够了就移动数据，否则就扩容）
     void ensure_write_space(uint64_t size)
     {
         // 如果末尾空闲空间大小足够，直接返回
@@ -134,20 +140,20 @@ public:
     }
 
     /////////////////////////////  读操作 /////////////////////////////
-    // 获取当前读取的起始地址
+    // 1. 获取当前读取的起始地址
     char* start_of_read()
     {
         return &(*_buffer.begin()) + _reader;
     }
 
-    // 获取可读数据的大小
+    // 2. 获取可读数据的大小
     uint64_t get_sizeof_read()
     {
         // 可读数据的大小 = 写偏移 - 读偏移
         return _writer - _reader;
     }
 
-    // 将读位置向后移动
+    // 3. 将读位置向后移动
     void push_reader_back(uint64_t size)
     {
         // 向后移动的大小，必须小于可读数据大小
@@ -155,7 +161,7 @@ public:
         _reader += size;
     }
 
-    // 读取数据操作（读取到buffer中）
+    // 4. 读取数据操作（读取到buffer中）
     void read_data(char* buffer, uint64_t size)
     {
         // 1. 要求要获取的数据大小必须小于可读数据大小
@@ -165,14 +171,14 @@ public:
         std::copy(start_of_read(), start_of_read() + size, (char*)buffer);
     }
 
-    // 读取数据操作（读取到buffer中），并将读取位置向后移动
+    // 5. 读取数据操作（读取到buffer中），并将读取位置向后移动
     void read_data_andMove(char* buffer, uint64_t size)
     {
         read_data(buffer, size);
         push_reader_back(size);
     }
 
-    // 读取数据后转化为string类型返回
+    // 6. 读取数据后转化为string类型返回
     std::string read_to_string(uint64_t size)
     {
         // 1. 要求要获取的数据大小必须小于可读数据大小
@@ -185,7 +191,7 @@ public:
         return str;
     }
 
-    // 读取数据后转化为string类型返回，并且让读取位置向后移动
+    // 7. 读取数据后转化为string类型返回，并且让读取位置向后移动
     std::string read_to_string_andMove(uint64_t size)
     {
         std::string str = read_to_string(size);
@@ -194,14 +200,14 @@ public:
     }
 
     /////////////////////////////  其它操作 /////////////////////////////
-    // 清空缓冲区接口
+    // 1. 清空缓冲区接口
     void clear_buffer()
     {
         // 只需要将偏移量置零即可
         _writer = _reader = 0;
     }
 
-    // 找到换行的位置（方便http解析）
+    // 2. 找到换行的位置（方便http解析）
     char* find_CRLF()
     {
         // 这里我们使用memchr找到\n就算找到换行位置
@@ -209,7 +215,7 @@ public:
         return res;
     }
     
-    // 获取一行数据（方便http解析）
+    // 3. 获取一行数据（方便http解析）
     std::string get_line()
     {
         char* pos = find_CRLF();
@@ -220,7 +226,7 @@ public:
         return read_to_string(pos - start_of_read() + 1);
     }
 
-    // 获取一行数据然后移动读取下标
+    // 4. 获取一行数据然后移动读取下标
     std::string get_line_andMove()
     {
         std::string str = get_line();
@@ -228,16 +234,205 @@ public:
         return str;
     }
 private:
-    // 获取缓冲区末尾空闲空间的大小 -- 即写位置之后的空闲空间
+    // 1. 获取缓冲区末尾空闲空间的大小 -- 即写位置之后的空闲空间
     uint64_t get_tail_free_size()
     {
         return _buffer.size() - _writer;
     }
 
-    // 获取缓冲区开头空闲空间的大小 -- 即读位置之前的空闲空间
+    // 2. 获取缓冲区开头空闲空间的大小 -- 即读位置之前的空闲空间
     uint64_t get_head_free_size()
     {
         return _reader;
+    }
+};
+
+const static int MAXBACKLOG = 1024;
+class Socket
+{
+private:
+    int _sockfd;
+public:
+    Socket() : _sockfd(-1) {}
+    Socket(int sockfd) : _sockfd(sockfd) {}
+    ~Socket() { Close(); };
+
+    // 1. 创建套接字
+    bool Create()
+    {
+        int n = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // IPPROTO_TCP指定的是TCP协议
+        if(n < 0)
+        {
+            ELOG("create socket error!!");
+            return false;
+        }
+        return true;
+    }
+
+    // 2. 绑定套接字信息
+    bool Bind(const std::string& ip, uint16_t port)
+    {
+        // 填充信息
+        struct sockaddr_in local;
+        memset(&local, 0, sizeof local);
+        local.sin_family = AF_INET;
+        local.sin_port = htons(port);
+        local.sin_addr.s_addr = inet_addr(ip.c_str());
+
+        // 绑定信息
+        int n = bind(_sockfd, (struct sockaddr*)&local, sizeof local);
+        if(n < 0)
+        {
+            ELOG("bind error!!");
+            return false;
+        }
+        return true;
+    }
+
+    // 3. 监听套接字
+    bool Listen(int backlog = MAXBACKLOG)
+    {
+        int n = listen(_sockfd, backlog);
+        if(n < 0)
+        {
+            ELOG("listen error!!");
+            return false;
+        }
+        return true;
+    }
+
+    // 4. 获取新连接
+    int Accept()
+    {
+        // 这里不关心获取到的新连接的属性
+        int newfd = accept(_sockfd, nullptr, nullptr);
+        if(newfd < 0)
+        {
+            ELOG("accept error!!");
+            return -1;
+        }
+        return newfd;
+    }
+
+    // 5. 客户端发起请求
+    bool Connect(const std::string& ip, uint16_t port)
+    {
+        // 填充信息
+        struct sockaddr_in client;
+        memset(&client, 0, sizeof client);
+        client.sin_family = AF_INET;
+        client.sin_port = htons(port);
+        client.sin_addr.s_addr = inet_addr(ip.c_str());
+
+        // 发起连接
+        int n = connect(_sockfd, (struct sockaddr*)&client, sizeof client);
+        if(n < 0)
+        {
+            ELOG("connect server error!!");
+            return false;
+        }
+        return true;
+    }
+
+    // 6. 接收数据
+    ssize_t Send(const void* buffer, size_t size, int flag = 0)
+    {
+        ssize_t n = send(_sockfd, buffer, size, flag);
+        if(n < 0)
+        {
+            // EAGAIN 表示当前socket的发送缓冲区满了，在非阻塞的情况下才会有这个错误
+            // EINTR  表示当前socket的阻塞等待，被信号打断了
+            if(errno == EAGAIN || errno == EINTR)
+                return 0;
+            ELOG("send error");
+            return -1;
+        }
+        return n; // 返回实际发送的数据长度
+    }
+
+    // 非等待式发送数据
+    ssize_t send_with_noblock(const void* buffer, size_t size)
+    {
+        if(size == 0)
+            return 0;
+        return Send(buffer, size, MSG_DONTWAIT); // MSG_DONTWAIT 表示当前接收为非阻塞
+    }
+
+    // 7. 发送数据
+    ssize_t Recv(void* buffer, size_t size, int flag = 0)
+    {
+        ssize_t n = recv(_sockfd, buffer, size, flag);
+        if(n < 0)
+        {
+            // EAGAIN 表示当前socket的接收缓冲区中没有数据了，在非阻塞的情况下才会有这个错误
+            // EINTR  表示当前socket的阻塞等待，被信号打断了
+            if(errno == EAGAIN || errno == EINTR)
+                return 0;
+            ELOG("recv error");
+            return -1;
+        }
+        return n; // 返回实际发送的数据长度
+    }
+
+    // 非等待式接收数据
+    ssize_t recv_with_noblock(void* buffer, size_t size)
+    {
+        return Recv(buffer, size, MSG_DONTWAIT); // MSG_DONTWAIT 表示当前接收为非阻塞
+    }
+
+    // 8. 关闭套接字
+    void Close()
+    {
+        if(_sockfd != -1)
+        {
+            close(_sockfd);
+            _sockfd = -1;
+        }
+    }
+
+    // 9. 创建一个服务端链接的接口
+    bool create_server(uint16_t port, const std::string& ip = "0.0.0.0")
+    {
+        // 1. 创建套接字  2. 绑定地址  3. 开始监听  4. 设置非阻塞  5. 启动地址重用
+        if(!Create()) 
+            return false;
+        if(!Bind(ip, port)) 
+            return false;
+        if(!Listen()) 
+            return false;
+        reuse_addr();
+        set_nonblock();
+        return true;
+    }
+
+    // 10. 创建一个客户端连接的接口
+    bool create_client(uint16_t port, const std::string& ip)
+    {
+        // 1. 创建套接字  2. 发起连接
+        if(!Create()) 
+            return false;
+        if(!Connect(ip, port))
+            return false;
+        return true;
+    }
+
+    // 11. 设置套接字选项 -- 开启地址端口复用
+    void reuse_addr()
+    {
+        // 设置地址复用
+        int val = 1;
+        setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof val);
+
+        // 设置端口复用
+        val = 1;
+        setsockopt(_sockfd, SOL_SOCKET, SO_REUSEPORT, &val, sizeof val);
+    }
+
+    // 12. 设置套接字阻塞属性 -- 设置为非阻塞
+    void set_nonblock()
+    {
+        int old = fcntl(_sockfd, F_GETFL);
+        fcntl(_sockfd, F_SETFL, old | O_NONBLOCK);
     }
 };
 
